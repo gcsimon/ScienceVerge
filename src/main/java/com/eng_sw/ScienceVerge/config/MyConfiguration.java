e1b47d1545cb09be58bdc923427b9f1e54a7af36
gcsimon <gio.simon@hotmail.com>
2020-11-16 20:25:16 -0300

    disabling cors security

--- src/main/java/com/eng_sw/ScienceVerge/config/MyConfiguration.java
+++ src/main/java/com/eng_sw/ScienceVerge/config/MyConfiguration.java
@@ -1,22 +1,16 @@
 package com.eng_sw.ScienceVerge.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MyConfiguration {
+@EnableWebMvc
+public class MyConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     }
 }
